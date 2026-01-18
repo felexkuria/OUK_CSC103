5fb93354359618718dc22c82395d68d0b0c3255b
Felex Kuria Irungu <engineerfelex@gmail.com>
2026-01-18 09:45:26 +0300

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -2,6 +2,12 @@
 int main(){
   int i=10;
   int *j=&i;
-  printf("This is value of i %d\n",i);
-  printf("This value of j %d\n",j);
+ 
+    printf("This is adress of i %d\n",&i);
+     printf("This value of j %d\n",j); 
+
+//   the value of i 
+ printf("This is value of i %d\n",i);
+   printf("This value of i through pointer  %d\n",*j);
+   
 }
