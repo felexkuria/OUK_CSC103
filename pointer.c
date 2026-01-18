a63e381dfd30b02d71eede16d9a26706fc3d6576
Felex Kuria Irungu <engineerfelex@gmail.com>
2026-01-18 10:19:14 +0300

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -2,9 +2,16 @@
 int main(){
   int i=10;
   int *j=&i;
+
+  int *l;
  
     printf("This is adress of i %d\n",&i);
+     printf("This is value of i %d\n",i);
+    printf("This memory address of l %p\n",l);
      printf("This value of j %d\n",j); 
+      printf("This address of j %p\n",j); 
+         printf("This address of i %p\n",&i); 
+           *j=23;
 
 //   the value of i 
  printf("This is value of i %d\n",i);
