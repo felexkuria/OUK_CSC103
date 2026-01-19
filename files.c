47a7be4a6824484f46dd7d84c6ec6c29228665d1
Felex Kuria Irungu <engineerfelex@gmail.com>
2026-01-19 06:29:43 +0300

    Update files.c

--- files.c
+++ files.c
@@ -1,1 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
 
+int main() {
+    FILE *filepointer;
+    filepointer=open("student1.txt","w");
+    if(filepointer==NULL){
+        printf(\n File Opening failed \n);
+        exit(1);
+    }
+    fprintf(filepointer,"%s \n %d","Joe Biden",81);
+     fprintf(filepointer,"%s \n %d","Kamala Harris",61);
+    fclose(filepointer);
+}
